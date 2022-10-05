063c9515ac7b8774eb5b39a06f5084b39c117ba2
vaguilar07 <113448472+vaguilar07@users.noreply.github.com>
2022-10-05 04:28:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int count = 1;
+    System.out.println("Printing Numbers from 1 to 10");
+    while (count <= 10) {
+      System.out.print(count++);
+      count = count + 1;
+    }
+  }
+}
